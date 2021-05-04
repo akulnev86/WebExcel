e1e36e4abb513ecf62a3e28c0152b2d7258e3e88
Alex <akulnev86@gmail.com>
2021-05-04 08:23:16 +0200

    Deleted unnecessary imports in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CloneBasenamePlugin } = require('enhanced-resolve');
-const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (env, argv) => {
 
