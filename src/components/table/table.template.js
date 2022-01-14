44601cfba23532e378d55aa85fd9ee06d5dec43f
Alex <akulnev86@gmail.com>
2022-01-14 15:46:34 +0100

    Row resize complete

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,91 +1,101 @@
 const CODES = {
-    A: 65,
-    Z: 90
-  }
-  
-  const DEFAULT_WIDTH = 120
-  
-  function getWidth(state, index) {
-    return (state[index] || DEFAULT_WIDTH) + 'px'
-  }
-  
-  function toCell(state, row) {
-    return function(_, col) {
-      const width = getWidth(state.colState, col)
-      return `
-        <div 
-          class="cell" 
-          contenteditable 
-          data-col="${col}"
-          data-type="cell"
-          data-id="${row}:${col}"
-          style="width: ${width}"
-        ></div>
-      `
-    }
-  }
-  
-  function toColumn({col, index, width}) {
+  A: 65,
+  Z: 90
+}
+
+const DEFAULT_WIDTH = 120
+const DEFAULT_HEIGHT = 24
+
+function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+
+function getHeight(state, index) {
+  return (state[index] || DEFAULT_HEIGHT) + 'px'
+}
+
+function toCell(state, row) {
+  return function(_, col) {
+    const width = getWidth(state, col)
     return `
       <div 
-        class="column" 
-        data-type="resizable" 
-        data-col="${index}" 
+        class="cell" 
+        contenteditable 
+        data-col="${col}"
+        data-type="cell"
+        data-id="${row}:${col}"
         style="width: ${width}"
-      >
-        ${col}
-        <div class="col-resize" data-resize="col"></div>
-      </div>
+      ></div>
     `
   }
-  
-  function createRow(index, content) {
-    const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
-    return `
-      <div class="row" data-type="resizable">
-        <div class="row-info">
-          ${index ? index : ''}
-          ${resize}
-        </div>
-        <div class="row-data">${content}</div>
+}
+
+function toColumn({col, index, width}) {
+  return `
+    <div 
+      class="column" 
+      data-type="resizable" 
+      data-col="${index}" 
+      style="width: ${width}"
+    >
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
+  `
+}
+
+function createRow(index, content, state) {
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+  const height = getHeight(state, index)
+  return `
+    <div 
+      class="row" 
+      data-type="resizable" 
+      data-row="${index}"
+      style="height: ${height}"
+    >
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
       </div>
-    `
-  }
-  
-  function toChar(_, index) {
-    return String.fromCharCode(CODES.A + index)
-  }
-  
-  function withWidthFrom(state) {
-    return function(col, index) {
-      return {
-        col, index, width: getWidth(state.colState, index)
-      }
+      <div class="row-data">${content}</div>
+    </div>
+  `
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index)
+}
+
+function withWidthFrom(state) {
+  return function(col, index) {
+    return {
+      col, index, width: getWidth(state.colState, index)
     }
   }
-  
-  export function createTable(rowsCount = 15, state = {}) {
-    const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
-    const rows = []
-  
-    const cols = new Array(colsCount)
+}
+
+export function createTable(rowsCount = 15, state = {}) {
+  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
+  const rows = []
+
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(withWidthFrom(state))
+      .map(toColumn)
+      .join('')
+
+  rows.push(createRow(null, cols, {}))
+
+  for (let row = 0; row < rowsCount; row++) {
+    const cells = new Array(colsCount)
         .fill('')
-        .map(toChar)
-        .map(withWidthFrom(state))
-        .map(toColumn)
+        .map(toCell(state.colState, row))
         .join('')
-  
-    rows.push(createRow(null, cols))
-  
-    for (let row = 0; row < rowsCount; row++) {
-      const cells = new Array(colsCount)
-          .fill('')
-          .map(toCell(state, row))
-          .join('')
-  
-      rows.push(createRow(row + 1, cells))
-    }
-  
-    return rows.join('')
+
+    rows.push(createRow(row + 1, cells, state.rowState))
   }
-  
+
+  return rows.join('')
+}
